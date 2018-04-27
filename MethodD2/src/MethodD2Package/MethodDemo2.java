9406d55f4e388b87d50789fb7b56afac27aee38d
Pratik <pratiquebangal@gmail.com>
2018-04-27 15:14:10 +0530

    Commit

---
+++ MethodD2/src/MethodD2Package/MethodDemo2.java
@@ -0,0 +1,18 @@
+package MethodD2Package;
+
+public class MethodDemo2 {
+	
+	public static void main(String[]args) {
+		
+		int b = addition();
+		System.out.println("Addition is " + b);
+	}
+	
+	static int addition()
+	{
+		int a = 10+20;
+		return a;
+		
+	}
+
+}
